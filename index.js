6d420f5756309f849b5656ca419feb114267cc25
pattysc <pattys@users.noreply.github.com>
2017-01-22 18:44:21 -0500

    oops

--- index.js
+++ index.js
@@ -55,3 +55,46 @@ Polygon.prototype.perimeter = function(){
 Polygon.prototype.numberOfSides = function(){
   return this.sides.length
 }
+
+function Quadrilateral(oneL, twoL, threeL, fourL){
+  Polygon.call(this, [new Side(oneL), new Side(twoL), new Side(threeL), new Side(fourL)])
+}
+
+Quadrilateral.prototype = Object.create(Polygon.prototype)
+Quadrilateral.prototype.constructor = Quadrilateral
+
+function Triangle(one, two, three){
+  Polygon.call(this, [new Side(one), new Side(two), new Side(three)])
+}
+
+Triangle.prototype = Object.create(Polygon.prototype)
+Triangle.prototype.constructor = Triangle
+
+function Rectangle(w, h){
+  this.width = w
+  this.height = h
+  Quadrilateral.call(this, w, h, w, h)
+}
+
+Rectangle.prototype = Object.create(Quadrilateral.prototype)
+Rectangle.prototype.constructor = Rectangle
+Rectangle.prototype.area = function(){
+  return this.width * this.height
+}
+
+function Square(length){
+  this.length = length
+  Rectangle.call(this, length, length)
+}
+
+Square.prototype = Object.create(Rectangle.prototype)
+Square.prototype.constructor = Square
+Square.prototype.listProperties = function(){
+  let string = ""
+  for (var prop in this) {
+    if(this.hasOwnProperty(prop)) {
+      string += prop
+    }
+  }
+  return string
+}
